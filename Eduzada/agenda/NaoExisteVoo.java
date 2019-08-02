8e6fb663a5f436ff17498d2789ec3e5494fbdb37
Deths <35713284+Deths@users.noreply.github.com>
2019-08-02 19:34:35 -0300

    teste

--- Eduzada/agenda/NaoExisteVoo.java
+++
@@ -1,7 +0,0 @@
-package agenda;
-
-public class NaoExisteVoo extends Exception {
-	public NaoExisteVoo() {
-		System.out.print("Esse voo não existe");
-	}
-}
--- Eduzada/usuario/Cliente.java
+++
@@ -1,7 +0,0 @@
-package usuario;
-
-public class Cliente extends Usuario{
-	public Cliente(String usuario, String senha) {
-		super(usuario, senha);
-	}
-}
